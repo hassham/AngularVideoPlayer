00a2b20e3c42370134b15c21e4d0dcd7bbfa69c3
Hasham <hasham87@hotmail.com>
2021-02-15 16:57:24 +0500

    commit with code

---
+++ src/app/pages/video-player-page/video-player-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { SSVideoSource, SSPlayerConfig, SSLoopType } from '../../common/components/ss-video-player/ss-video-player.component';
+
+@Component({
+    templateUrl: 'video-player-page.html'
+})
+export class VideoPlayerPageComponent implements OnInit {
+    playerConfig: SSPlayerConfig = {
+        loop: SSLoopType.All,
+        autoplay: false,
+        trackUser: true
+    };
+    videoSources: SSVideoSource[] = [];
+
+    ngOnInit(): void {
+        this.videoSources = [
+            {
+                title: 'Big Buck Bunny - 2008',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-bigbuckbunny.jpg',
+                source: 'https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.ogg'
+            },
+            {
+                title: 'Sintel - 2010',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-sintel.jpg',
+                source: 'http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.1080p.mkv'
+            },
+            {
+                title: 'Tears of Steel - 2012',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-tearsofsteel.jpg',
+                source: 'http://media.xiph.org/mango/tears_of_steel_1080p.webm'
+            }
+        ];
+    }
+}
